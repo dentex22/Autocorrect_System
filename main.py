1a2c6733e05fd8e59e7812adafa8d74e96791d6e
dentex <83866386+dentex22@users.noreply.github.com>
2021-11-20 14:31:54 +0300

    minor edits

--- main.py
+++ main.py
@@ -11,16 +11,16 @@ import pandas as pd
 # 3) Returns a list of words.
 
 
-words = []
+w = [] #words
 with open('sample.txt','r',encoding="utf8") as f:
     file_name_data = f.read()
     file_name_data = file_name_data.lower()
-    words = re.findall('\w+', file_name_data)
+    w = re.findall('\w+', file_name_data)
 
 
-vocab = set(words)
-print(f"The first ten words in the text are: \n{words[0:10]}")
-print(f"There are {len(vocab)} unique words in the vocabulary.")
+v = set(w) #vocabulary
+print(f"The first 10 words in our dictionary are: \n{w[0:10]}")
+print(f"The dictionary has {len(v)} words ")
 
 
 # a get_count function that returns a dictionary of word vs frequency
@@ -34,7 +34,7 @@ def get_count(words):
     return word_count_dict
 
 
-word_count_dict = get_count(words)
+word_count_dict = get_count(w)
 print(f"There are {len(word_count_dict)} key values pairs")
 
 
@@ -51,25 +51,22 @@ def get_probs(word_count_dict):
 
 
 # Now we implement 4 edit word functions
-# delete_letter: given a word, it returns all the possible strings that have one character removed.
-# switch_letter: given a word, it returns all the possible strings that have two adjacent letters switched.
-# replace_letter: given a word, it returns all the possible strings that have one character replaced by another different letter.
-# insert_letter: given a word, it returns all the possible strings that have an additional character inserted.
 
-def delete_letter(word):
-    delete_l = []
-    split_l = []
+# DeleteLetter:removes a letter from a given word
+def DeleteLetter(word):
+    delete_list = []
+    split_list = []
     for i in range(len(word)):
-        split_l.append((word[0:i], word[i:]))
-    for a, b in split_l:
-        delete_l.append(a + b[1:])
-    return delete_l
+        split_list.append((word[0:i], word[i:]))
+    for a, b in split_list:
+        delete_list.append(a + b[1:])
+    return delete_list
 
 
-delete_word_l = delete_letter(word="cans")
+delete_word_l = DeleteLetter(word="cans")
 
-
-def switch_letter(word):
+# SwitchLetter:swap two adjacent letters
+def SwitchLetter(word):
     split_l = []
     switch_l = []
     for i in range(len(word)):
@@ -78,72 +75,72 @@ def switch_letter(word):
     return switch_l
 
 
-switch_word_l = switch_letter(word="eta")
+switch_word_l = SwitchLetter(word="eta")
 
-
+# replace_letter: changes one letter to another
 def replace_letter(word):
     split_l = []
-    replace_l = []
+    replace_list = []
     for i in range(len(word)):
         split_l.append((word[0:i], word[i:]))
-    letters = 'abcdefghijklmnopqrstuvwxyz'
-    replace_l = [a + l + (b[1:] if len(b) > 1 else '') for a, b in split_l if b for l in letters]
-    return replace_l
+    alphabets = 'abcdefghijklmnopqrstuvwxyz'
+    replace_list = [a + l + (b[1:] if len(b) > 1 else '') for a, b in split_l if b for l in alphabets]
+    return replace_list
 
 
 replace_l = replace_letter(word='can')
 
-
+# insert_letter: adds additional characters
 def insert_letter(word):
     split_l = []
-    insert_l = []
+    insert_list = []
     for i in range(len(word) + 1):
         split_l.append((word[0:i], word[i:]))
     letters = 'abcdefghijklmnopqrstuvwxyz'
-    insert_l = [a + l + b for a, b in split_l for l in letters]
+    insert_list = [a + l + b for a, b in split_l for l in letters]
     # print(split_l)
-    return insert_l
+    return insert_list
 
 
 
 # combining the edits
 # switch operation optional
 def edit_one_letter(word, allow_switches=True):
-    edit_one_set = set()
-    edit_one_set.update(delete_letter(word))
+    edit_set1 = set()
+    edit_set1.update(DeleteLetter(word))
     if allow_switches:
-        edit_one_set.update(switch_letter(word))
-    edit_one_set.update(replace_letter(word))
-    edit_one_set.update(insert_letter(word))
-    return edit_one_set
+        edit_set1.update(SwitchLetter(word))
+    edit_set1.update(replace_letter(word))
+    edit_set1.update(insert_letter(word))
+    return edit_set1
 
 
 
 # edit two letters
 def edit_two_letters(word, allow_switches=True):
-    edit_two_set = set()
+    edit_set2 = set()
     edit_one = edit_one_letter(word, allow_switches=allow_switches)
     for w in edit_one:
         if w:
             edit_two = edit_one_letter(w, allow_switches=allow_switches)
-            edit_two_set.update(edit_two)
-    return edit_two_set
+            edit_set2.update(edit_two)
+    return edit_set2
 
 
 
 # get corrected word
 def get_corrections(word, probs, vocab, n=2):
-    suggestions = []
-    n_best = []
-    suggestions = list(
+    suggested_word = []
+    best_suggestion = []
+    suggested_word = list(
         (word in vocab and word) or edit_one_letter(word).intersection(vocab) or edit_two_letters(word).intersection(
             vocab))
-    n_best = [[s, probs[s]] for s in list(reversed(suggestions))]
-    return n_best
+    best_suggestion = [[s, probs[s]] for s in list(reversed(suggested_word))]
+    return best_suggestion
 
 
-my_word = input("Enter a word:")
+my_word = input("Enter any word:")
 probs = get_probs(word_count_dict)
-tmp_corrections = get_corrections(my_word, probs, vocab, 2)
+tmp_corrections = get_corrections(my_word, probs, v, 2)
 for i, word_prob in enumerate(tmp_corrections):
     print(f"word {i}: {word_prob[0]}, probability {word_prob[1]:.6f}")
